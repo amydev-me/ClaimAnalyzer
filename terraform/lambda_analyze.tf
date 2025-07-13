b95d8f7d7415ba82bc52537f09f38ecf11b67f47
Amy <ampyaephyonaing@gmail.com>
2025-07-13 17:57:51 +0800

    Update handler

--- terraform/lambda_analyze.tf
+++ terraform/lambda_analyze.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_function" "submission_analyzer" {
   function_name = "${var.project_name}-submission-analyzer"
-  handler       = "submission_analyzer.lambda_handler"
+  handler       = "lambda_function.lambda_handler"
   runtime       = "python3.12"
   role          = aws_iam_role.lambda_exec.arn
 
