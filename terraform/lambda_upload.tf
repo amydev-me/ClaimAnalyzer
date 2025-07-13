0dd143ec6f6a46c2ebb00e8bd92285aede0e3d2f
Amy <ampyaephyonaing@gmail.com>
2025-07-13 18:07:41 +0800

    Update handler

--- terraform/lambda_upload.tf
+++ terraform/lambda_upload.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_function" "upload_handler" {
   function_name = "${var.project_name}-upload-handler"
-  handler       = "lambda_function.lambda_handler"
+  handler       = "upload_handler.lambda_handler"
   runtime       = "python3.12"
   role          = aws_iam_role.lambda_exec.arn
 
